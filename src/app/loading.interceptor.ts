be955c28d461cdfcba9d4ab888f16aefc54d1497
ukml2018 <ukmlcal@gmail.com>
2024-08-02 10:56:19 +0530

    2nd Release

---
+++ src/app/loading.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable, finalize } from 'rxjs';
+import { LoadingService } from './services/loading.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private loadingService: LoadingService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loadingService.show();
+
+    return next.handle(req).pipe(
+      finalize(() => this.loadingService.hide())
+    );
+  }
+}
