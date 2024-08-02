85ea4705cbd005de14a678babce70b8e5987174b
ukml2018 <ukmlcal@gmail.com>
2024-08-02 10:39:48 +0530

    First release

---
+++ src/app/services/query.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable, output } from "@angular/core";
+
+@Injectable({
+    providedIn:"root"
+})
+
+export class QueryService {
+
+    constructor(private http:HttpClient) {
+
+    }
+    
+    // url = 'http://169.63.181.214:5000/invokesearch/';
+    url = 'http://150.240.3.0:5555/invokesearch/';
+
+
+    callConverse(query:string) {
+        
+    const finalURL = `${this.url}${query}`;
+
+        return this.http.get(finalURL);
+    } 
+}
